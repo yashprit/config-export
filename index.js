85c0aa4ab70327f87bd2a647327ed309407e8e78
Priyanka Bhoir <priyanka.bhoir@rocketmail.com>
2015-03-30 16:12:21 +0530

    Update index.js
    reverted back to previous version

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 'use strict';
 
 var
-  fs = require('fs'),
-  path = require('path'),
-  _ = require('');
+  fs   = require('fs'),
+  path = require('path');
 
-module.exports = function(dir) {
-  var args = Array.prototype.splice.call(arguments);
-  //first argument has to be dir
-  var dir = args[0];
-
-  //second argument could be files that need to be exluded from system
-  var exludedFiles = args[1];
-  var requires = {};
-
-  if (!dir) {
+module.exports = function (dir) {
+  if(!dir) {
     throw new Error("No Directory provided to load files");
   }
-
-  //get all files from provided directory
+  var requires = {};
   var files = fs.readdirSync(dir);
 
-  if (exludedFiles) {
-    files = _.difference(files, exludedFiles);
-  }
-
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     // ignore index.js and files prefixed with underscore and
     if (!(file === 'index.js' || file[0] === '_')) {
 
